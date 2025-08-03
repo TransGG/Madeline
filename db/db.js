ca7679f64b6e164acedc3ba73bb450e8ab50323c
hyperneutrino <hyperneutrino15@gmail.com>
2025-08-03 17:39:06 -0400

    set up mongodb database

---
+++ db/db.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require("mongodb");
+const { mongodbURI } = require("../config/config.json");
+
+const MONGO_CLIENT_PROMISE = new MongoClient(mongodbURI).connect();
+const DATABASE_PROMISE = MONGO_CLIENT_PROMISE.then((CLIENT) => CLIENT.db("Madeline"));
+
+module.exports = {
+    async getRoles(group) {
+        const DATABASE = await DATABASE_PROMISE;
+        const entry = await DATABASE.collection("role_groups").findOne({ key: group });
+        return entry?.roles ?? [];
+    },
+    async setRoles(group, roles) {
+        const DATABASE = await DATABASE_PROMISE;
+        await DATABASE.collection("role_groups").updateOne({ key: group }, { $set: { roles } }, { upsert: true });
+    }
+};
