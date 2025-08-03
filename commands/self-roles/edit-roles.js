f14bc644c077287a152945760aaa224b6680ef8f
hyperneutrino <hyperneutrino15@gmail.com>
2025-08-03 17:39:06 -0400

    add handler for command to edit roles

--- commands/self-roles/edit-roles.js
+++ commands/self-roles/edit-roles.js
@@ -1,6 +1,110 @@
 const {
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
   ApplicationCommandOptionType,
 } = require("discord.js");
+const { options } = require("../../config/roles");
+
+exports.slash = async (client, interaction) => {
+  const subcommand = interaction.options.getSubcommand(true);
+
+  if (subcommand == "add-role") {
+    const role = interaction.options.getRole("role", true);
+    
+    if (role.comparePositionTo(role.guild.members.me.roles.highest) >= 0) {
+      await interaction.reply({
+        content: "This role is too high up for this bot to assign.",
+        ephemeral: true,
+      });
+
+      return;
+    }
+
+    const emojiR = interaction.options.getString("emoji", true);
+    const menuEmojiR = interaction.options.getString("menu-emoji", false) ?? "";
+
+    const emoji = emojiR.match(/^<a?:[^:]+:\d+>$/) ? emojiR.split(":")[2].slice(0, -1) : emojiR;
+    const menuEmoji = menuEmojiR.match(/^<a?:[^:]+:\d+>$/) ? menuEmojiR.split(":")[2].slice(0, -1) : menuEmojiR;
+
+    if (emoji.includes("|") || menuEmoji.includes("|")) {
+      await interaction.reply({
+        content: "A provided emoji is invalid and appears to break parsing.",
+        ephemeral: true,
+      });
+
+      return;
+    }
+
+    await interaction.reply({
+      content: "Choose the role group to which to add the new role option.",
+      ephemeral: true,
+      components: [
+        new ActionRowBuilder().addComponents(
+          new StringSelectMenuBuilder()
+            .setCustomId(`edit-roles|add-role|${role.id}|${emoji}|${menuEmoji}`)
+            .setPlaceholder("Choose A Role Group")
+            .addOptions(options.map((option) => ({
+              label: option.label,
+              value: option.value,
+              description: option.description,
+              emoji: option.emoji,
+            }))),
+        ),
+      ],
+    });
+  } else if (subcommand == "remove-role") {
+    await interaction.reply({
+      content: "Choose the role group from which to remove the new role option.",
+      ephemeral: true,
+      components: [
+        new ActionRowBuilder().addComponents(
+          new StringSelectMenuBuilder()
+            .setCustomId("edit-roles|remove-role")
+            .setPlaceholder("Choose A Role Group")
+            .addOptions(options.map((option) => ({
+              label: option.label,
+              value: option.value,
+              description: option.description,
+              emoji: option.emoji,
+            }))),
+        ),
+      ],
+    });
+  } else if (subcommand == "update-role") {
+    const emojiR = interaction.options.getString("emoji", false) ?? "";
+    const menuEmojiR = interaction.options.getString("menu-emoji", false) ?? "";
+
+    const emoji = emojiR.match(/^<a?:[^:]+:\d+>$/) ? emojiR.split(":")[2].slice(0, -1) : emojiR;
+    const menuEmoji = menuEmojiR.match(/^<a?:[^:]+:\d+>$/) ? menuEmojiR.split(":")[2].slice(0, -1) : menuEmojiR;
+
+    if (emoji.includes("|") || menuEmoji.includes("|")) {
+      await interaction.reply({
+        content: "A provided emoji is invalid and appears to break parsing.",
+        ephemeral: true,
+      });
+
+      return;
+    }
+
+    await interaction.reply({
+      content: "Choose the role group to which the role option to edit belongs.",
+      ephemeral: true,
+      components: [
+        new ActionRowBuilder().addComponents(
+          new StringSelectMenuBuilder()
+            .setCustomId(`edit-roles|update-role|${emoji}|${menuEmoji}`)
+            .setPlaceholder("Choose A Role Group")
+            .addOptions(options.map((option) => ({
+              label: option.label,
+              value: option.value,
+              description: option.description,
+              emoji: option.emoji,
+            }))),
+        ),
+      ],
+    });
+  }
+};
 exports.setup = async (client, guilds) => {
   guilds.map((guild) =>
     guild.commands.create({
